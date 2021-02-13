64868d9901571d0fec05f5de22386e8ea1a53ec7
IvanKozlov98 <ivan1998olimp@yandex.ru>
2021-02-13 07:05:34 -0500

    hw1 solved

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,8 +1,10 @@
 module Parser where
 
-import Control.Applicative ((<|>))
-import Data.Char (isDigit, digitToInt)
-import Text.Printf (printf)
+import           Control.Applicative            ( (<|>) )
+import           Data.Char                      ( isDigit
+                                                , digitToInt
+                                                )
+import           Text.Printf                    ( printf )
 
 data Operator = Plus
               | Mult
@@ -13,7 +15,7 @@ toOp :: Char -> Operator
 toOp '+' = Plus
 toOp '*' = Mult
 toOp '^' = Pow
-toOp c = error $ printf "Unsupported operator: %c" c
+toOp c   = error $ printf "Unsupported operator: %c" c
 
 data Expr = BinOp Operator Expr Expr
           | Num Int
@@ -22,33 +24,31 @@ data Expr = BinOp Operator Expr Expr
 eval :: Expr -> Int
 eval (BinOp Plus l r) = eval l + eval r
 eval (BinOp Mult l r) = eval l * eval r
-eval (Num x) = x
+eval (BinOp Pow  l r) = eval l ^ eval r
+eval (Num x         ) = x
 
 data ParserType = Prefix | Infix deriving (Show)
 
 parse :: ParserType -> String -> Maybe Expr
-parse pType str =
-    case go pType str of
-      Just ("", e) -> Just e
-      _ -> Nothing
-  where
-    go Prefix = parsePrefix
-    go Infix  = parseInfix
+parse pType str = case go pType str of
+  Just ("", e) -> Just e
+  _            -> Nothing
+ where
+  go Prefix = parsePrefix
+  go Infix  = parseInfix
 
 -- Expr :: + Expr Expr
 --       | * Expr Expr
 --       | Digit
 parsePrefix :: String -> Maybe (String, Expr)
-parsePrefix (op : t) | op == '+' || op == '*' =
+parsePrefix (op : t) | op == '+' || op == '*' || op == '^' =
   case parsePrefix t of
-    Just (t', l) ->
-      case parsePrefix t' of
-        Just (t'', r) -> Just (t'', BinOp (toOp op) l r)
-        Nothing -> Nothing
+    Just (t', l) -> case parsePrefix t' of
+      Just (t'', r) -> Just (t'', BinOp (toOp op) l r)
+      Nothing       -> Nothing
     Nothing -> Nothing
-parsePrefix (d : t) | isDigit d =
-  Just (t, Num (digitToInt d))
-parsePrefix _ = Nothing
+parsePrefix (d : t) | isDigit d = Just (t, Num (digitToInt d))
+parsePrefix _                   = Nothing
 
 -- Expr :: Expr + Expr
 --       | Expr * Expr
@@ -58,79 +58,75 @@ parsePrefix _ = Nothing
 -- Expr :: Слаг + Слаг + ... + Слаг
 --       = Слаг (+ Слаг) (+ Слаг) .. (+ Слаг)
 --       -> [Слаг] - fold ... -> BinOp Plus (BinOp Plus ...)...
--- Слаг :: Множ * Множ * ... * Множ
+-- Слаг :: Factor * Factor * ... * Factor
+-- Factor :: Множ ^ Factor | Множ 
 -- Множ :: Цифра | ( Expr )
 -- [1,2,3] -> (1+2)+3
 
-binOp :: Operator -> [Expr] -> Expr
-binOp op = foldl1 (BinOp op)
+data Associativity = LeftA | RightA
+binOp :: (Expr -> Expr -> Expr) -> Associativity -> [Expr] -> Expr
+binOp appOp LeftA  = foldl1 appOp
+binOp appOp RightA = foldr1 appOp
 
 parseInfix :: String -> Maybe (String, Expr)
 parseInfix = parseSum
 
 parseSum :: String -> Maybe (String, Expr)
-parseSum str =
-    (binOp Plus <$>) <$> go str
-  where
-    go :: String -> Maybe (String, [Expr])
-    go str =
-      let first = parseMult str in
-      case first of
-        Nothing -> Nothing
-        Just (t, e) ->
-          if null t
-          then Just ("", [e])
-          else
-            case parsePlus t of
-              Just (t', _) ->
-                let rest = go t' in
-                ((e:) <$>) <$> rest
-              Nothing -> Just (t, [e])
+parseSum = parseAny (binOp plus LeftA) parseMult parsePlus
 
 parseMult :: String -> Maybe (String, Expr)
-parseMult str =
-    (binOp Mult <$>) <$> go str
-  where
-    go :: String -> Maybe (String, [Expr])
-    go str =
-      let first = parseDigit str <|> parseExprBr str in
-      case first of
-        Nothing -> Nothing
-        Just (t, e) ->
-          if null t
-          then Just ("", [e])
-          else
-            case parseStar t of
-              Just (t', _) ->
-                let rest = go t' in
-                ((e:) <$>) <$> rest
-              Nothing -> Just (t, [e])
+parseMult = parseAny (binOp mult LeftA) parsePow parseStar
 
 parsePow :: String -> Maybe (String, Expr)
-parsePow str = undefined
+parsePow = parseAny (binOp pow RightA)
+                    (\str -> parseDigit str <|> parseExprBr str)
+                    parseHat
+
+parseAny
+  :: ([Expr] -> Expr)
+  -> -- binApp
+     (String -> Maybe (String, Expr))
+  -> -- firstFun
+     (String -> Maybe (String, Operator))
+  -> --parseSign
+     String
+  -> Maybe (String, Expr)
+parseAny binApp firstFun parseSign str = (binApp <$>) <$> go str
+ where
+  go :: String -> Maybe (String, [Expr])
+  go str =
+    let first = firstFun str
+    in  case first of
+          Nothing     -> Nothing
+          Just (t, e) -> if null t
+            then Just ("", [e])
+            else case parseSign t of
+              Just (t', _) -> let rest = go t' in ((e :) <$>) <$> rest
+              Nothing      -> Just (t, [e])
+
 
 parseExprBr :: String -> Maybe (String, Expr)
-parseExprBr ('(' : t) =
-  case parseSum t of
-    Just ((')' : t'), e) -> Just (t', e)
-    _ -> Nothing
+parseExprBr ('(' : t) = case parseSum t of
+  Just (')' : t', e) -> Just (t', e)
+  _                  -> Nothing
 parseExprBr _ = Nothing
 
+parseOperator :: Char -> String -> Maybe (String, Operator)
+parseOperator op (t : ts) | op == t = Just (ts, toOp op)
+parseOperator _ _                   = Nothing
+
 parsePlus :: String -> Maybe (String, Operator)
-parsePlus ('+' : t) = Just (t, Plus)
-parsePlus _ = Nothing
+parsePlus = parseOperator '+'
 
 parseStar :: String -> Maybe (String, Operator)
-parseStar ('*' : t) = Just (t, Mult)
-parseStar _ = Nothing
+parseStar = parseOperator '*'
 
 parseHat :: String -> Maybe (String, Operator)
-parseHat str = undefined
+parseHat = parseOperator '^'
 
 parseDigit :: String -> Maybe (String, Expr)
-parseDigit (d : t) | isDigit d =
-  Just (t, Num (digitToInt d))
-parseDigit _ = Nothing
+parseDigit (d : t) | isDigit d = Just (t, Num (digitToInt d))
+parseDigit _                   = Nothing
 
 plus :: Expr -> Expr -> Expr
 plus = BinOp Plus
