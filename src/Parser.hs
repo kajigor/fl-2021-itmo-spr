d8ad8e326a54236828c16ec62e1eba346163efa2
Anton Klochkov <tklochkov@gmail.com>
2021-02-13 21:16:50 +0300

    Completed tasks

--- src/Parser.hs
+++ src/Parser.hs
@@ -22,6 +22,7 @@ data Expr = BinOp Operator Expr Expr
 eval :: Expr -> Int
 eval (BinOp Plus l r) = eval l + eval r
 eval (BinOp Mult l r) = eval l * eval r
+eval (BinOp Pow l r) = eval l ^ eval r
 eval (Num x) = x
 
 data ParserType = Prefix | Infix deriving (Show)
@@ -39,16 +40,17 @@ parse pType str =
 --       | * Expr Expr
 --       | Digit
 parsePrefix :: String -> Maybe (String, Expr)
-parsePrefix (op : t) | op == '+' || op == '*' =
-  case parsePrefix t of
-    Just (t', l) ->
-      case parsePrefix t' of
-        Just (t'', r) -> Just (t'', BinOp (toOp op) l r)
-        Nothing -> Nothing
-    Nothing -> Nothing
-parsePrefix (d : t) | isDigit d =
-  Just (t, Num (digitToInt d))
-parsePrefix _ = Nothing
+parsePrefix (s : t) 
+  | s == '+' || s == '*' || s == '^' =
+    case parsePrefix t of
+      Just (t', l) ->
+        case parsePrefix t' of
+          Just (t'', r) -> Just (t'', BinOp (toOp s) l r)
+          Nothing -> Nothing
+      Nothing -> Nothing
+  | isDigit s = Just (t, Num (digitToInt s))
+parsePrefix _ = Nothing 
+
 
 -- Expr :: Expr + Expr
 --       | Expr * Expr
@@ -62,70 +64,64 @@ parsePrefix _ = Nothing
 -- Множ :: Цифра | ( Expr )
 -- [1,2,3] -> (1+2)+3
 
-binOp :: Operator -> [Expr] -> Expr
-binOp op = foldl1 (BinOp op)
+data Associativity = L | R;
+
+binOp :: Operator -> Associativity -> [Expr] -> Expr
+binOp op L = foldl1 (BinOp op)
+binOp op R = foldr1 (BinOp op)
 
 parseInfix :: String -> Maybe (String, Expr)
-parseInfix = parseSum
+parseInfix = parseByOperation Plus
 
-parseSum :: String -> Maybe (String, Expr)
-parseSum str =
-    (binOp Plus <$>) <$> go str
+operatorMapInner :: Operator -> Maybe Operator
+operatorMapInner Plus = Just Mult
+operatorMapInner Mult = Just Pow
+operatorMapInner Pow = Nothing 
+
+operatorToParser :: Operator -> String -> Maybe (String, Operator)
+operatorToParser Plus = parseStringOperator '+'
+operatorToParser Mult = parseStringOperator '*'
+operatorToParser Pow = parseStringOperator '^'
+
+operatorAssociativity :: Operator -> Associativity
+operatorAssociativity Plus = L
+operatorAssociativity Mult = L
+operatorAssociativity Pow = R 
+
+parseByOperation :: Operator -> String -> Maybe (String, Expr)
+parseByOperation operator str = 
+    (binOp operator (operatorAssociativity operator) <$>) <$> go str operator
   where
-    go :: String -> Maybe (String, [Expr])
-    go str =
-      let first = parseMult str in
-      case first of
-        Nothing -> Nothing
-        Just (t, e) ->
-          if null t
-          then Just ("", [e])
-          else
-            case parsePlus t of
+    execOperation str operator = case operatorMapInner operator of 
+          (Just subOp) -> parseByOperation subOp str
+          Nothing -> parseDigit str <|> parseExprBr str
+    go :: String -> Operator -> Maybe (String, [Expr])
+    go str operator = 
+      let first = execOperation str operator in 
+        case first of 
+          Nothing -> Nothing
+          Just (t, e) ->
+            if null t then 
+              Just ("", [e])
+            else
+            case operatorToParser operator t of
               Just (t', _) ->
-                let rest = go t' in
-                ((e:) <$>) <$> rest
+                let rest = go t' operator in
+                  ((e:) <$>) <$> rest
               Nothing -> Just (t, [e])
-
-parseMult :: String -> Maybe (String, Expr)
-parseMult str =
-    (binOp Mult <$>) <$> go str
-  where
-    go :: String -> Maybe (String, [Expr])
-    go str =
-      let first = parseDigit str <|> parseExprBr str in
-      case first of
-        Nothing -> Nothing
-        Just (t, e) ->
-          if null t
-          then Just ("", [e])
-          else
-            case parseStar t of
-              Just (t', _) ->
-                let rest = go t' in
-                ((e:) <$>) <$> rest
-              Nothing -> Just (t, [e])
-
-parsePow :: String -> Maybe (String, Expr)
-parsePow str = undefined
 
 parseExprBr :: String -> Maybe (String, Expr)
 parseExprBr ('(' : t) =
-  case parseSum t of
-    Just ((')' : t'), e) -> Just (t', e)
+  case parseInfix t of
+    Just (')' : t', e) -> Just (t', e)
     _ -> Nothing
 parseExprBr _ = Nothing
 
-parsePlus :: String -> Maybe (String, Operator)
-parsePlus ('+' : t) = Just (t, Plus)
-parsePlus _ = Nothing
-
-parseStar :: String -> Maybe (String, Operator)
-parseStar ('*' : t) = Just (t, Mult)
-parseStar _ = Nothing
-
-parseHat :: String -> Maybe (String, Operator)
-parseHat str = undefined
+parseStringOperator :: Char -> String -> Maybe (String, Operator)
+parseStringOperator op [] = Nothing 
+parseStringOperator op (x : t) 
+  | op == x = Just (t, toOp x)
+  | otherwise = Nothing 
 
 parseDigit :: String -> Maybe (String, Expr)
 parseDigit (d : t) | isDigit d =
