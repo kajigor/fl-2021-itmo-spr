af4ba485d1ffa3a74284b2a8254106b5911e8122
Valeria <vkolmogorova@techmategroup.com>
2021-02-17 23:05:28 +0300

    solved task 1, no refactoring

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,5 +1,6 @@
 module Parser where
 
+import Prelude hiding (Left, Right) 
 import Control.Applicative ((<|>))
 import Data.Char (isDigit, digitToInt)
 import Text.Printf (printf)
@@ -22,6 +23,7 @@ data Expr = BinOp Operator Expr Expr
 eval :: Expr -> Int
 eval (BinOp Plus l r) = eval l + eval r
 eval (BinOp Mult l r) = eval l * eval r
+eval (BinOp Pow l r) = eval l ^ eval r
 eval (Num x) = x
 
 data ParserType = Prefix | Infix deriving (Show)
@@ -37,10 +39,11 @@ parse pType str =
 
 -- Expr :: + Expr Expr
 --       | * Expr Expr
+--       | ^ Expr Expr
 --       | Digit
 parsePrefix :: String -> Maybe (String, Expr)
-parsePrefix (op : t) | op == '+' || op == '*' =
-  case parsePrefix t of
+parsePrefix (op : t) | op == '+' || op == '*' || op == '^' =
+  case parsePrefix t of 
     Just (t', l) ->
       case parsePrefix t' of
         Just (t'', r) -> Just (t'', BinOp (toOp op) l r)
@@ -52,6 +55,7 @@ parsePrefix _ = Nothing
 
 -- Expr :: Expr + Expr
 --       | Expr * Expr
+--       | Expr ^ Expr
 --       | Digit
 --       | ( Expr )
 
@@ -59,18 +63,22 @@ parsePrefix _ = Nothing
 --       = Слаг (+ Слаг) (+ Слаг) .. (+ Слаг)
 --       -> [Слаг] - fold ... -> BinOp Plus (BinOp Plus ...)...
 -- Слаг :: Множ * Множ * ... * Множ
--- Множ :: Цифра | ( Expr )
+-- Множ :: Выр ^ Выр ^ ... ^ Выр
+-- Выр :: Цифра | ( Expr )
 -- [1,2,3] -> (1+2)+3
 
-binOp :: Operator -> [Expr] -> Expr
-binOp op = foldl1 (BinOp op)
+data Associative = Left | Right
+
+binOp :: Associative -> Operator -> [Expr] -> Expr
+binOp Left op = foldl1 (BinOp op)
+binOp Right op = foldr1 (BinOp op)
 
 parseInfix :: String -> Maybe (String, Expr)
 parseInfix = parseSum
 
 parseSum :: String -> Maybe (String, Expr)
 parseSum str =
-    (binOp Plus <$>) <$> go str
+    (binOp Left Plus <$>) <$> go str
   where
     go :: String -> Maybe (String, [Expr])
     go str =
@@ -89,11 +97,11 @@ parseSum str =
 
 parseMult :: String -> Maybe (String, Expr)
 parseMult str =
-    (binOp Mult <$>) <$> go str
+    (binOp Left Mult <$>) <$> go str
   where
     go :: String -> Maybe (String, [Expr])
     go str =
-      let first = parseDigit str <|> parseExprBr str in
+      let first = parsePow str in
       case first of
         Nothing -> Nothing
         Just (t, e) ->
@@ -107,7 +115,23 @@ parseMult str =
               Nothing -> Just (t, [e])
 
 parsePow :: String -> Maybe (String, Expr)
-parsePow str = undefined
+parsePow str =
+    (binOp Right Pow <$>) <$> go str
+  where
+    go :: String -> Maybe (String, [Expr])
+    go str =
+      let first = parseDigit str <|> parseExprBr str in
+      case first of
+        Nothing -> Nothing
+        Just (t, e) ->
+          if null t
+          then Just ("", [e])
+          else
+            case parseHat t of
+              Just (t', _) ->
+                let rest = go t' in
+                ((e:) <$>) <$> rest
+              Nothing -> Just (t, [e])
 
 parseExprBr :: String -> Maybe (String, Expr)
 parseExprBr ('(' : t) =
@@ -125,7 +149,8 @@ parseStar ('*' : t) = Just (t, Mult)
 parseStar _ = Nothing
 
 parseHat :: String -> Maybe (String, Operator)
-parseHat str = undefined
+parseHat ('^' : t) = Just (t, Pow)
+parseHat _ = Nothing
 
 parseDigit :: String -> Maybe (String, Expr)
 parseDigit (d : t) | isDigit d =
