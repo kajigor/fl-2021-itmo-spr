ae4ad9b43447f204f3fd28109d473d8704a24b1d
Ekaterina Ledeneva <ekiuled@gmail.com>
2021-02-28 21:34:31 +0300

    uberExpr

--- src/Parser/Common.hs
+++ src/Parser/Common.hs
@@ -2,7 +2,7 @@ module Parser.Common where
 
 import Data.Char (isDigit, isAlpha)
 import Parser.Combinators ( Parser, satisfy )
-import Control.Applicative (some)
+import Control.Applicative ( Alternative(..), liftA2 )
 
 digit :: Parser String Char
 digit = satisfy isDigit
@@ -25,8 +25,35 @@ data Associativity = LeftAssoc  -- x `op` y `op` z == (x `op` y) `op` z
                                 -- x `op` y -- ok
                                 -- x `op` y `op` z -- not ok
 
+sepByOp :: Parser input op -> Parser input elem -> Parser input ([op], [elem])
+sepByOp sep elem = do
+  h <- elem
+  t <- many (liftA2 (,) sep elem)
+  return (map fst t, h : map snd t)
+
 uberExpr :: [(Parser i op, Associativity)]
          -> Parser i ast
          -> (op -> ast -> ast -> ast)
          -> Parser i ast
-uberExpr = undefined
+uberExpr [] atom _ = atom
+uberExpr ((op,LeftAssoc):ops) atom build = do
+  (ops,atoms) <- sepByOp op (uberExpr ops atom build)
+  let ini = head atoms
+  let elems = tail atoms
+  let fs = zipWith flip (map build ops) elems
+  return $ foldl (flip (.)) id fs ini
+uberExpr ((op,RightAssoc):ops) atom build = do
+  (ops,atoms) <- sepByOp op (uberExpr ops atom build)
+  let ini = last atoms
+  let elems = init atoms
+  let fs = zipWith ($) (map build ops) elems
+  return $ foldr (.) id fs ini
+uberExpr ((op,NoAssoc):ops) atom build = 
+  let elem = uberExpr ops atom build in
+  ( do
+    lhs <- elem 
+    o <- op 
+    rhs <- elem 
+    return $ build o lhs rhs
+  ) <|>
+  elem
