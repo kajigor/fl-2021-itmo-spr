ff4edcd341f86634c06d792f0e889cd95ce7e18b
Anton Klochkov <tklochkov@gmail.com>
2021-03-01 00:04:44 +0300

    Completed haskell

--- src/Parser/Common.hs
+++ src/Parser/Common.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
 module Parser.Common where
 
-import Data.Char (isDigit, isAlpha)
-import Parser.Combinators ( Parser, satisfy )
-import Control.Applicative (some)
+import Data.Char (isDigit, isAlpha, isAlphaNum)
+import Parser.Combinators ( Parser, satisfy ) 
+import Control.Applicative (some, Alternative((<|>), many))
 
 digit :: Parser String Char
 digit = satisfy isDigit
@@ -19,6 +20,13 @@ number = do
   num <- some digit
   return $ (read num :: Int)
 
+-- number :: not empty, consists of digits
+varName :: Parser String String
+varName = do
+  firstLetter <- alpha
+  otherPart <- many $ satisfy (\char -> isAlphaNum char || char == '_')
+  return $ firstLetter : otherPart
+
 data Associativity = LeftAssoc  -- x `op` y `op` z == (x `op` y) `op` z
                    | RightAssoc -- x `op` y `op` z == x `op` (y `op` z)
                    | NoAssoc    -- non associative operation:
@@ -29,4 +37,15 @@ uberExpr :: [(Parser i op, Associativity)]
          -> Parser i ast
          -> (op -> ast -> ast -> ast)
          -> Parser i ast
-uberExpr = undefined
+uberExpr [] parser _ = parser
+uberExpr ((parser, assoc) : xs) defPars func = case assoc of 
+  NoAssoc -> flip func <$> subParser <*> parser <*> subParser <|> subParser 
+  LeftAssoc -> 
+    do 
+      (applyValue <$>) $ fmap (,) subParser <*> many (fmap (flip . func) parser <*> subParser)
+  RightAssoc ->
+    do 
+      (applyValue <$>) $ fmap (\(s, a) -> (a, reverse s)) $ fmap (,) (many $fmap (flip func) subParser <*> parser) <*> subParser
+  where 
+    subParser = uberExpr xs defPars func
+    applyValue (start, arr) = foldl (\l f -> f l) start arr
--- src/Parser/Infix.hs
+++ src/Parser/Infix.hs
@@ -1,12 +1,42 @@
 {-# LANGUAGE LambdaCase #-}
 module Parser.Infix where
 
-import Expr ( Expr )
-import Parser.Combinators ( Parser, eof, whitespaces, parseEither )
+import Expr ( Expr(..), Operator, toOp ) 
+import Parser.Combinators ( Parser, eof, whitespaces, parseEither, word, symbol )
+import Parser.Common
+    ( Associativity(RightAssoc, NoAssoc, LeftAssoc),
+      number,
+      uberExpr,
+      varName )
+import Control.Applicative (Alternative((<|>)))
 
 parse :: String -> Either String Expr
 parse =
   parseEither (whitespaces *> parseExpr <* whitespaces <* eof)
 
+parseCompare :: Parser String Operator
+parseCompare = toOp <$> word "==" <|> toOp <$> word "/=" <|> toOp <$> word "<=" <|> toOp <$> word "<" <|> toOp <$> word ">=" <|> toOp <$> word ">"
+
+parsePM :: Parser String Operator
+parsePM = toOp <$> word "+" <|> toOp <$> word "-"
+
+parseMD :: Parser String Operator
+parseMD = toOp <$> word "*" <|> toOp <$> word "/"
+
+parseP :: Parser String Operator
+parseP = toOp <$> word "^"
+
+parseDefault :: Parser String Expr
+parseDefault = Ident <$> varName <|> Num <$> number <|> symbol '(' *> parseExpr <* symbol ')'
+
+parserWhitespaces :: Parser String a -> Parser String a
+parserWhitespaces parser = whitespaces *> parser <* whitespaces
+
 parseExpr :: Parser String Expr
-parseExpr = undefined
+parseExpr = uberExpr [ 
+  (parserWhitespaces parseCompare, NoAssoc),
+  (parserWhitespaces parsePM, LeftAssoc),
+  (parserWhitespaces parseMD, LeftAssoc),
+  (parserWhitespaces parseP, RightAssoc)]
+  (parserWhitespaces parseDefault)
+  BinOp
