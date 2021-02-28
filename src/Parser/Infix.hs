1e589c43f52c5bc5e3dc01695e7d189a94f42f6e
Ekaterina Ledeneva <ekiuled@gmail.com>
2021-02-28 23:52:34 +0300

    Infix parser

--- src/Parser/Infix.hs
+++ src/Parser/Infix.hs
@@ -1,12 +1,47 @@
 {-# LANGUAGE LambdaCase #-}
 module Parser.Infix where
 
-import Expr ( Expr )
-import Parser.Combinators ( Parser, eof, whitespaces, parseEither )
+import Control.Applicative ((<|>), many)
+import Parser.Combinators ( Parser, eof, whitespaces, parseEither, word, symbol )
+import Parser.Common ( Associativity (..), uberExpr, number, alpha, digit )
+import Expr ( Operator(..), Expr (..), toOp )
 
 parse :: String -> Either String Expr
 parse =
   parseEither (whitespaces *> parseExpr <* whitespaces <* eof)
 
+mult, sum', minus, div', pow :: Parser String Operator
+mult  = toOp <$> word "*"
+sum'  = toOp <$> word "+"
+minus = toOp <$> word "-"
+div'  = toOp <$> word "/"
+pow   = toOp <$> word "^"
+
+eq', neq, ge, gt, le, lt :: Parser String Operator
+eq'   = toOp <$> word "=="
+neq   = toOp <$> word "/="
+ge    = toOp <$> word ">="
+gt    = toOp <$> word ">"
+le    = toOp <$> word "<="
+lt    = toOp <$> word "<"
+
+ident :: Parser String String
+ident = do
+  first <- alpha <|> symbol '_'
+  rest <- many (alpha <|> digit <|> symbol '_')
+  return (first : rest)
+
 parseExpr :: Parser String Expr
-parseExpr = undefined
+parseExpr = 
+  uberExpr [ (eq' <|> neq <|> ge <|> gt <|> le <|> lt, NoAssoc)
+           , (sum' <|> minus, LeftAssoc)
+           , (mult <|> div', LeftAssoc)
+           , (pow, RightAssoc)
+           ]
+           ( whitespaces *> (
+               Num <$> number
+           <|> Ident <$> ident
+           <|> symbol '(' *> parseExpr <* symbol ')'
+             <* whitespaces )
+           )
+           BinOp
