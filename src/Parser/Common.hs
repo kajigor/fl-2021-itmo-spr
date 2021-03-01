ef82e25bc050a77247e9fb2d733131855041f774
IvanKozlov98 <ivan1998olimp@yandex.ru>
2021-02-28 23:18:26 -0500

    minor

--- src/Parser/Common.hs
+++ src/Parser/Common.hs
@@ -24,33 +24,12 @@ data Associativity = LeftAssoc  -- x `op` y `op` z == (x `op` y) `op` z
                    | NoAssoc    -- non associative operation:
                                 -- x `op` y -- ok
                                 -- x `op` y `op` z -- not ok
-{-
-uberExpr :: [(Parser i op, Associativity)]  -- список парсеров бинарных операторов с ассоциативностями в порядке повышения приоритета
-         -> Parser i ast -- парсер для элементарного выражения
-         -> (op -> ast -> ast -> ast) -- функция для создания абстрактного синтаксического дерева для бинарного оператора.
-         -> Parser i ast -- результирующий парсер
-uberExpr ((opPr, assoc): ps) exprPr fOp = do
-    exprs <- sepBy opPr exprPr
-    let 
-      ff = \x
-      resExpr = case assoc of
-        LeftAssoc -> foldl1 fOp
-        RightAssoc -> foldr1 fOp
-        NoAssoc -> foldl1 fOp -- todo
--}
-
--- binOp :: Associativity -> Operator -> [Expr] -> Expr
-helper'' LeftAssoc fOp op  = foldl1 (fOp op)
-helper'' RightAssoc fOp op = foldr1 (fOp op)
-helper'' NoAssoc fOp op = foldr1 (fOp op)
 
 uberExpr :: [(Parser i op, Associativity)]  -- список парсеров бинарных операторов с ассоциативностями в порядке повышения приоритета
          -> Parser i ast -- парсер для элементарного выражения
          -> (op -> ast -> ast -> ast) -- функция для создания абстрактного синтаксического дерева для бинарного оператора.
          -> Parser i ast -- результирующий парсер
 uberExpr [] exprPr f = exprPr
-
-
 uberExpr ((parseOp, assoc): ps) exprPr fOp = do
   tmp <|> do {nextParser}
     where
@@ -60,12 +39,11 @@ uberExpr ((parseOp, assoc): ps) exprPr fOp = do
         case assoc of
             NoAssoc -> do
               op <- parseOp
-              expr2 <- nextParser
-              return $ fOp op expr1 expr2 
+              fOp op expr1 <$> nextParser
             _ -> do
               op_expr <- some ((,) <$> parseOp <*> nextParser)
               case assoc of
-                LeftAssoc -> return $ foldl (\xx el -> fOp (fst el) xx (snd el)) expr1 op_expr
+                LeftAssoc -> return $ foldl (\e1 (op, e2) -> fOp op e1 e2) expr1 op_expr
                 RightAssoc -> return $ helper expr1 op_expr
                   where
                     helper e [] = e
