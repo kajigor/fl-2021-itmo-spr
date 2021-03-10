1525ce3e1625f57ea2887fa034d0fd32fee74bb3
zhvkgj <zhvkgj@yandex.ru>
2021-03-10 20:35:04 +0300

    add syntax notation and printer

--- AstStmt.hs
+++ AstStmt.hs
@@ -32,5 +32,51 @@ data Stmt = Ignore Expr               -- Инструкция, которая я
 -- Абстрактное синтаксическое дерево программы
 data Program = Program Stmt -- Программа является инструкцией
 
+toOp :: Operator -> String
+toOp Pow = "^"
+toOp Mult = "*"
+toOp Div = "/"
+toOp Plus = "+"
+toOp Minus = "-" 
+toOp Eq = "=="
+toOp Neq = "!="
+toOp Le = "<="
+toOp Lt = "<"
+toOp Ge = ">="
+toOp Gt = ">"
+
+printExpr :: Expr -> String
+printExpr (Ident sname) = sname
+printExpr (Num num) = show num
+printExpr (BinOp op ex1 ex2) =
+    let op' = toOp op
+        ex1' = printExpr ex1
+        ex2' = printExpr ex2
+    in ex1' ++ " " ++ op' ++ " " ++ ex2'
+
+printIf :: Expr -> Stmt -> Maybe Stmt -> String
+printIf ex stmt1 (Just stmt2) = "if (" ++ printExpr ex ++ ") " 
+        ++ printStmt stmt1 ++ "else " 
+        ++ printStmt stmt2
+printIf ex stmt1 Nothing = "if (" ++ printExpr ex ++ ") " 
+        ++ printStmt stmt1 
+
+printWhile :: Expr -> Stmt -> String
+printWhile ex stmt = "while (" ++ printExpr ex ++ ") " 
+    ++ printStmt stmt
+
+printStmt :: Stmt -> String
+printStmt stmt =
+    case stmt of
+        Ignore ex         -> printExpr ex ++ ";"
+        If ex stmt1 stmt2 -> printIf ex stmt1 stmt2
+        While ex stmt     -> printWhile ex stmt           
+        Read var          -> "read " ++ var                 
+        Write ex          -> "write " ++ printExpr ex       
+        Assign var ex     -> var ++ " = " ++ printExpr ex         
+        Seq xs            -> "{" ++ foldr (\stmt str -> printStmt stmt ++ " " ++ str) "}" xs
+
 printer :: Program -> String
-printer = undefined
+printer (Program stmt) = printStmt stmt
+
+test1 = Program $ Seq [Read "a", While (BinOp Le (Ident "a") (Num 100)) (Seq [Write (Ident "a"), Assign "a" (BinOp Minus (Ident "a") (Num 1))])]
