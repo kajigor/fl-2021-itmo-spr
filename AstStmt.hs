97496513c672aae1b63831868820e99ffb1976ef
anvar <anvar5710@mail.ru>
2021-03-10 23:14:48 +0300

    Tasks

--- AstStmt.hs
+++ AstStmt.hs
@@ -15,10 +15,25 @@ data Operator = Pow   -- Возведение в степень
               | Ge    -- Больше или равно
               | Gt    -- Больше
 
+instance Show Operator where
+    show Pow = "**"
+    show Mult = "*"
+    show Div = "/"
+    show Plus = "+"
+    show Minus = "-"
+    show Eq = "=="
+    show Neq = "^="
+    show Le = "<="
+    show Lt = "<"
+    show Ge = ">="
+    show Gt = ">"
+
 -- Выражения (expressions)
 data Expr = Ident Var                -- Идентификатор
           | Num Int                  -- Число
           | BinOp Operator Expr Expr -- Выражение с бинарным оператором
+
+
 
 -- Инструкции (statements)
 data Stmt = Ignore Expr               -- Инструкция, которая является выражением (подразумевается, что значение выражения игнорируется)
@@ -29,8 +44,34 @@ data Stmt = Ignore Expr               -- Инструкция, которая я
           | Assign Var Expr           -- Присвоить значение выражения переменной
           | Seq [Stmt]                -- Последовательность инструкций
 
+instance Show Stmt where
+    show (Ignore x) = show x ++ ";\n"
+
+    show (If cond t f) = "if(" ++ show cond ++ "){\n" ++ show t ++"\n}" ++ case f of
+        Nothing -> ""
+        Just altbody -> "else{\n" ++ show altbody ++ "\n}" 
+
+    show (While cond body) = "while(" ++ show cond ++ "){\n" ++ show body ++"\n}"  
+
+    show (Read v) = "input(" ++ v ++ ");\n" 
+    show (Write e) = " print(" ++ show e ++ ");\n"
+
+    show (Assign param exp) = "auto " ++ param ++ " = " ++ show exp ++ ";\n"
+
+    show (Seq (x:rest)) = show x ++ "\n" ++ show (Seq rest)
+    show (Seq []) = ""
+
+instance Show Expr where
+    show (Ident v) = v
+    show (Num i) = show i
+    show (BinOp op e1 e2) = "(" ++ show e1 ++ show op ++ show e2 ++ ")"
+
 -- Абстрактное синтаксическое дерево программы
 data Program = Program Stmt -- Программа является инструкцией
 
+
+instance Show Program where
+    show (Program stmt) = show stmt
+
 printer :: Program -> String
-printer = undefined
+printer = show
