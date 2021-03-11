1b4654e5e8bf04f3b7eb0d4dc74666d9dce000a3
valeria <urgbal@gmail.com>
2021-03-11 05:47:51 +0300

    tasks

--- AstStmt.hs
+++ AstStmt.hs
@@ -33,6 +33,11 @@ data Expr = Ident Var                -- Идентификатор
           | Num Int                  -- Число
           | BinOp Operator Expr Expr -- Выражение с бинарным оператором
 
+instance Show Expr where
+    show (Ident var) = var
+    show (Num x) = show x
+    show (BinOp op expr1 expr2) = "(" ++ show expr1 ++ ")" ++ show op ++ "(" ++ show expr2 ++ ")"
+
 -- Инструкции (statements)
 data Stmt = Ignore Expr               -- Инструкция, которая является выражением (подразумевается, что значение выражения игнорируется)
           | If Expr Stmt (Maybe Stmt) -- Условное выражение. Первый операнд -- условие, второй -- ветка true, третий -- опциональная ветка else
@@ -42,12 +47,37 @@ data Stmt = Ignore Expr               -- Инструкция, которая я
           | Assign Var Expr           -- Присвоить значение выражения переменной
           | Seq [Stmt]                -- Последовательность инструкций
 
+instance Show Stmt where
+  show (Ignore expr) = show expr ++ ";\n"
+
+  show (If expr trueBranch elseBranch) = "if (" ++ show expr ++ ") begin\n " ++ show trueBranch ++ " end\n" ++ (elseShow elseBranch)
+    where elseShow Nothing = ""
+          elseShow (Just stmt) = " else begin\n " ++ show stmt ++ " end\n"
+
+  show (While expr stmt) = "while (" ++ show expr ++ ") begin\n " ++ show stmt ++ " end\n"
+  show (Read var) = ">> " ++ var ++ ";\n"
+  show (Write expr) = "<< " ++ show expr ++ ";\n"
+  show (Assign var expr) = var ++ " = " ++ show expr ++ ";\n"
+
+  show (Seq []) = ""
+  show (Seq (x:xs)) = show x ++ show (Seq xs)
+
 -- Абстрактное синтаксическое дерево программы
 data Program = Program Stmt -- Программа является инструкцией
 
 printer :: Program -> String
-printer (Program s) = printerStmt s
+printer (Program s) = show s
 
-printerStmt :: Stmt -> String
+-- Пример
+readStmt = Read "a"
 
-printerExpr :: Expr -> String
+ifStmt = If condition (Seq [Assign "x" (Num 100)]) (Just (Seq [Assign "x" (BinOp Minus (Ident "x") (Num 100))]))
+  where condition = BinOp Ge (Ident "x") (Num 10)
+
+whileStmt = While condition (Seq [Write (Ident "x")])
+  where condition = Num 1
+
+stmtSeq = Seq [readStmt, ifStmt, whileStmt]
+
+main = do
+  putStr (printer $ Program stmtSeq)
--- language.md
+++ language.md
@@ -8,25 +8,23 @@
 
 Пробелы разделяют слова языка. Отступы не имеют никакого смысла кроме визуального удобства. После каждого выражения стоит ';'.
 
-
+В условном выражении число <=0 считается как false, > 0 - как true.
 
 **Идентификаторы**
-'if', 'while', 'or', 'else', 'and', 'begin', 'end', 'for', 'not', 'function', 'true', 'false' etc.
+`if`, `while`, `or`, `else`, `and`, `begin`, `end`, `for`, `not`, `function`, `true`, `false` etc.
 
-num :: '-'? ('0' | ... | '9')+, целое число
-var :: любое слово над алфавитом
-op :: '+' | '-' | '*' | '/' 
-term :: '(' term op term ')' | var | num
-statement :: sequence | if | while | assign | read | print
-sequence :: statement ';' | sequence+
-if :: 'if' '(' statement ')' 'begin' sequence 'end'
-condition :: term boolean term | not term
-boolean :: or | and | equals | not equals
-while :: 'while' '(' condition ')' 'begin' sequence 'end'
-if :: 'if' '(' condition ')' 'begin' sequence 'end'
-assign :: var '=' term
-or :: '||'
-and :: '&&'
-equals :: '=='
-not equals :: '!='
-not  :: '!'
+`if` :: 'if' '(' term ')' 'begin' sequence 'end' 'else' 'begin' sequence 'end'; ветки 'else' может не быть
+`while` :: 'while' '(' term ')' 'begin' sequence 'end'
+`assign` :: var '=' term
+`read` :: '>>' var
+`print` :: '<<' term
+
+`sequence` :: statement ';' | sequence+
+`statement` :: term | if | while | assign | read | print
+`term` :: '(' term ')' | term binop term | not term | var | num
+
+`num` :: '-'? ('0' | ... | '9')+, целое число
+`var` :: любое слово над алфавитом
+
+`binop` :: '^' | '*' | '/' | '+' | '-' | '<=' | '<' | '=>' | '>' | '==' | '!='
+`not`  :: '!'
